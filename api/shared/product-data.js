974717dc4c177b018cd77d6a76f244a1726ced7a
Justen <justen@outlook.com>
2023-03-24 18:19:11 -0600

    Updated data

--- api/shared/product-data.js
+++ api/shared/product-data.js
@@ -30,6 +30,72 @@ const data = {
       description: 'Shiny',
       quantity: 1,
     },
+    {
+      id: 70,
+      name: 'Heavy Crossbow',
+      description: 'PEW PEW',
+      quantity: 2,
+    },
+    {
+      id: 80,
+      name: 'Short sword',
+      description: 'Stabitty stab',
+      quantity: 5,
+    },
+    {
+      id: 90,
+      name: 'Light Crossbow',
+      description: 'pew pew pew',
+      quantity: 1,
+    },
+    {
+      id: 100,
+      name: 'Dagger',
+      description: 'Rogues do it from behind',
+      quantity: 8,
+    },
+    {
+      id: 110,
+      name: 'Pound of Fire snake scales',
+      description: '1,000,000,000 Scoville',
+      quantity: 2,
+    },
+    {
+      id: 120,
+      name: 'Caltrops',
+      description: 'Ow ow ow ow ow ow...',
+      quantity: 60,
+    },
+    {
+      id: 130,
+      name: 'Grappling Hook',
+      description: 'Rope not included',
+      quantity: 3,
+    },
+    {
+      id: 140,
+      name: 'Vial of Basilisk Saliva',
+      description: 'Accept no substitute',
+      quantity: 3,
+    },
+    {
+      id: 150,
+      name: 'Magical Purple Armband',
+      description: '???',
+      quantity: 3,
+    },
+    {
+      id: 160,
+      name: 'Pearl',
+      description: '100g each',
+      quantity: 3,
+    },
+    {
+      id: 170,
+      name: 'Party Coin',
+      description: '5 Platinum, 772 Gold, 3 Silver',
+      quantity: 822.3,
+    }
   ],
 };
 
